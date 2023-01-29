6a7ffa051cbfc2f94affde2896fcef50ec32a2ac
Etanjo <99751108+Etanjo@users.noreply.github.com>
2023-01-29 01:03:43 +0000

    Added patch notes link and instructions

--- app/menu.ts
+++ app/menu.ts
@@ -3,9 +3,13 @@ import {setupPregame} from './cardList'
 let game = document.querySelector('#game')
 let startButton = document.querySelector('#start')
 let menu = document.querySelector('#menu')
+let instructions = document.querySelector('#instructions')
 
 startButton.addEventListener('click', function(event){
   menu.classList.toggle('hidden')
   game.classList.toggle('hidden')
   setupPregame()
+})
+instructions.addEventListener('click',function(event){
+  instructions.classList.toggle('active')
 })
